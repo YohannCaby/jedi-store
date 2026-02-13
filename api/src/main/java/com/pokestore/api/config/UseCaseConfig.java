e912d9f4ce7667a64194fee7383df60ad0246ff9
Yohann CABY <yohann.caby@groupe-creative.fr>
2026-02-13 22:27:45 +0100

    tech : springboot upgrade

--- api/src/main/java/com/pokestore/api/config/UseCaseConfig.java
+++ api/src/main/java/com/pokestore/api/config/UseCaseConfig.java
@@ -9,7 +9,7 @@ import com.pokestore.core.port.out.ProductRepositoryPort;
 import com.pokestore.core.service.CustomerService;
 import com.pokestore.core.service.OrderService;
 import com.pokestore.core.service.ProductService;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
